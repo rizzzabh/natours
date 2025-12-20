c348296db5f2bcbbde19f1f8d1864719a9c28173
rizzzabh <rishabhmishra963010@gmail.com>
2025-12-20 23:05:29 +0530

    document middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const slugify = require("slugify");
 
 const TourSchema = new mongoose.Schema(
   {
@@ -6,6 +7,9 @@ const TourSchema = new mongoose.Schema(
       type: String,
       required: true,
       unique: true,
+    },
+    slug: {
+      type: String,
     },
     rating: Number,
     duration: {
@@ -38,6 +42,15 @@ const TourSchema = new mongoose.Schema(
 TourSchema.virtual("durationWeeks").get(function () {
   return this.duration / 7;
 });
+// runs on save() and create() command , not on insertMany() ;
+
+TourSchema.pre("save", function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  //console.log(this);
+  next();
+});
+
+/// TourSchema.post("save" , function(doc , next)){}
 
 const Tour = mongoose.model("Tour", TourSchema);
 
