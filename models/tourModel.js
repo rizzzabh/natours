cb4f020adf58724a9446e1eaf2a180b13a8dfa5d
rizzzabh <rishabhmishra963010@gmail.com>
2025-12-20 22:44:09 +0530

    added options for virtuals properties

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,33 +1,39 @@
 const mongoose = require("mongoose");
 
-const TourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    unique: true,
+const TourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    rating: Number,
+    duration: {
+      type: String,
+    },
+    ratingsAverage: Number,
+    ratingsQuantity: Number,
+    priceDiscount: Number,
+    summary: String,
+    maxGroupSize: {
+      type: Number,
+    },
+    difficulty: Number,
+    description: String,
+    price: {
+      type: Number,
+      required: true,
+    },
+    imageCover: {
+      type: String,
+    },
+    images: [String],
   },
-  rating: Number,
-  duration: {
-    type: String,
-  },
-  ratingsAverage: Number,
-  ratingsQuantity: Number,
-  priceDiscount: Number,
-  summary: String,
-  maxGroupSize: {
-    type: Number,
-  },
-  difficulty: Number,
-  description: String,
-  price: {
-    type: Number,
-    required: true,
-  },
-  imageCover: {
-    type: String,
-  },
-  images: [String],
-});
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
 
 TourSchema.virtual("durationWeeks").get(function () {
   return this.duration / 7;
